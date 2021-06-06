55671b34533139d46075b656f484a268ccdf3a1f
totolastiko <76597788+totolastiko@users.noreply.github.com>
2021-06-06 22:58:06 +0200

    Update test_imu.py

--- test_imu.py
+++ test_imu.py
@@ -3,12 +3,14 @@ from nanpy.i2c import I2C_Master
 import math
 from time import sleep as sleep
 
-#L'arduino a une taille limité de données contrairement à python
-#donc on limite à 16 bits
+# L'arduino a une taille limité de données contrairement à python
+# donc on doit limiter à 16 bits les données pour repliquer ce que fait 
+# arduino
 def ulong_to_long(u):
     ''' using subtraction '''
     return u - (1<<16) if u >= (1<<15) else u
 
+#connection à l'arduino
 try:
     connection = SerialManager(device="com3")
     a = ArduinoApi(connection =connection)
@@ -16,18 +18,18 @@ try:
 except:
     print('Error: No Arduino found...')
 
+#fonction qui map la valeurs reçu en angle
 def _map(x, in_min, in_max, out_min, out_max):
     return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
-   
-MPU_address = 0x68   
-master = I2C_Master(wire.Wire(connection = connection))  
-master.send(MPU_address, [0x6B,0])
+
+MPU_address = 0x68  # Definition d'adresse
+master = I2C_Master(wire.Wire(connection = connection)) #connection I2C
+master.send(MPU_address, [0x6B,0]) #registre PWR_MGMT_1 et envoie zero (pour réveiller le MPU-6050)
 
 while (True):
-
     master.send(MPU_address, [0x3B])
-    data = master.request(MPU_address, 14)
-
+    data = master.request(MPU_address, 14) #on lie les données
+    # puis on les interprètes
     accX = ulong_to_long(data[0] << 8) | data[1]
     accY = ulong_to_long(data[2] << 8) | data[3]
     accZ = ulong_to_long(data[4] << 8) | data[5]
